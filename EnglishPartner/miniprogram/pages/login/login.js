b137c0af0921fbab051f4a55ae358f2f56fcd8d9
konggujushi2012 <30611569+konggujushi2012@users.noreply.github.com>
2018-10-31 23:59:44 +0800

    [CHG]:add view wxss style

--- EnglishPartner/miniprogram/pages/login/login.js
+++ EnglishPartner/miniprogram/pages/login/login.js
@@ -1,5 +1,25 @@
 page({
   data:{
-    canIUse:wx.canIUse('button.open-type.getUserInfo')
-  }
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              //从数据库获取用户信息
+              that.queryUsreInfo();
+              //用户已经授权过
+              wx.switchTab({
+                url: ''
+              })
+            }
+          });
+        }
+      }
+    })
+  },
 })
